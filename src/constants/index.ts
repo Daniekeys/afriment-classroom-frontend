c7fcec769416e09a1846e40932ebf9fbfcc5d51f
Daniekeys <starlordflash@gmail.com>
2026-03-13 06:37:25 +0100

    fixed the recommendation

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,5 @@
-export const DEPARTMENT = ['CS', 'Math', 'English', 'History'];
+export const DEPARTMENT = ["CS", "Math", "English", "History"];
 
 export const DEPARTMENT_OPTIONS = [
-  
-  ...DEPARTMENT.map((dept) => ({ value: dept, label: dept }))
+  ...DEPARTMENT.map((dept) => ({ value: dept, label: dept })),
 ];
-
